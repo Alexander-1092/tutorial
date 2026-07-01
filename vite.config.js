a480619389dca90d72fb3a5ca0bdcdb65c2d7103
Alexander <Alex-Sadr1092@yandex.ru>
2026-07-01 14:49:12 +0300

    feat: puth base

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,37 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-export default defineConfig({
-  base: "/tutorial/dist/",
-  root: ".",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "main", "index.html"),
-        sass: resolve(__dirname, "SASS", "sass.html"),
-        tutorCSS: resolve(__dirname, "tutorCSS", "index.html"),
-        vite: resolve(__dirname, "vite", "vite.html"),
-        VSCode: resolve(__dirname, "VSCode", "index.html"),
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === "build";
+
+  return {
+    base: isBuild ? "/tutorial/dist/" : "/",
+    root: ".",
+    server: {
+      open: "/main/index.html",
+    },
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "main", "index.html"),
+          sass: resolve(__dirname, "SASS", "sass.html"),
+          tutorCSS: resolve(__dirname, "tutorCSS", "index.html"),
+          vite: resolve(__dirname, "vite", "vite.html"),
+          VSCode: resolve(__dirname, "VSCode", "index.html"),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      "@scss": resolve(__dirname, "./src/scss"),
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        "@scss": resolve(__dirname, "./src/scss"),
+      },
     },
-  },
-  plugins: [
-    ViteImageOptimizer({
-      disable: process.env.NODE_ENV !== "production",
-    }),
-  ],
+    plugins: [
+      ViteImageOptimizer({
+        disable: process.env.NODE_ENV !== "production",
+      }),
+    ],
+  };
 });
