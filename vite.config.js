913969a83aacff170649a5e742bcb59adeb44e88
Alexander <Alex-Sadr1092@yandex.ru>
2026-09-08 15:23:51 +0300

    feat: add git

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,6 @@ export default defineConfig(({ command, mode }) => {
           vite: resolve(__dirname, "vite", "vite.html"),
           VSCode: resolve(__dirname, "VSCode", "index.html"),
           git: resolve(__dirname, "git", "index.html"),
-          git: resolve(__dirname, "form", "form.html"),
         },
       },
     },
